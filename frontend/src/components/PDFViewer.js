c477d0cfde047650729db215ee13bf98fd8fce37
hwoo7449 <hwoo7449@naver.com>
2024-06-24 20:37:48 +0900

    pdf 기본 레이아웃 추가

--- frontend/src/components/PDFViewer.js
+++ frontend/src/components/PDFViewer.js
@@ -1,17 +1,26 @@
+import React from 'react';
 import { Worker, Viewer } from '@react-pdf-viewer/core';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
 const PDFViewer = ({ url }) => {
+    const defaultLayoutPluginInstance = defaultLayoutPlugin({
+        sidebarTabs: defaultTabs => [
+            // 탭 순서 변경, 삭제 또는 추가
+            defaultTabs[0], // 썸네일 탭 유지
+            // defaultTabs[1] 또는 [2]는 삭제하여 북마크와 첨부 파일 탭 제거
+        ]
+    });
     return (
         <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
             <div
-                style={{
-                    border: '1px solid rgba(0, 0, 0, 0.3)',
-                    width: '800px',
-                    height: '800px',
-                }}
+                style={{ width: '800px', height: '800px', margin: 'auto'}}
             >
-                <Viewer fileUrl={url} />
+                <Viewer
+                    fileUrl={url}
+                    plugins={[defaultLayoutPluginInstance]}
+                    />
             </div>
         </Worker>
     );
